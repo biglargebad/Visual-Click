37347400791c78d584110fb25c113151586ea997
biglargebad <106358531+biglargebad@users.noreply.github.com>
2022-12-12 15:01:19 -0600

    This one works
    alright cool. Straight up Blink app

--- examples/LinkHut/LinkHut/ContentView.swift
+++ examples/LinkHut/LinkHut/ContentView.swift
@@ -3,6 +3,8 @@
 import SwiftUI
 import AVFoundation
 import AudioToolbox.AudioServices
+//import GoogleMobileAds
+//import UIKit
 
 private let fontSize = 30.0
 private let imageSize = 35.0
@@ -17,9 +19,74 @@ private var switchBool = false
 private var switchBoolLight = false
 private var colorSwitchBool = false
 private var screenOff = false
+/*
+struct BannerAd: UIViewRepresentable {
 
+    var unitID: String
 
+    func makeCoordinator() -> Coordinator {
+        // For Implementing Delegates..
+        return Coordinator()
+    }
 
+    func makeUIView(context: Context) -> GADBannerView{
+        let adView = GADBannerView(adSize: GADAdSizeBanner)
+
+        adView.adUnitID = unitID
+        adView.rootViewController = UIApplication.shared.getRootViewController()
+
+        adView.load(GADRequest())
+
+        return adView
+    }
+
+    func updateUIView(_ uiView: GADBannerView, context: Context) {
+
+    }
+
+    class Coordinator: NSObject, GADBannerViewDelegate {
+        func bannerViewDidReceiveAd(_ bannerView: GADBannerView) {
+            print("bannerViewDidReceiveAd")
+        }
+
+        func bannerView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: Error) {
+            print("bannerView:didFailToReceiveAdWithError: \(error.localizedDescription)")
+        }
+
+        func bannerViewDidRecordImpression(_ bannerView: GADBannerView) {
+            print("bannerViewDidRecordImpression")
+        }
+
+        func bannerViewWillPresentScreen(_ bannerView: GADBannerView) {
+            print("bannerViewWillPresentScreen")
+        }
+
+        func bannerViewWillDismissScreen(_ bannerView: GADBannerView) {
+            print("bannerViewWillDIsmissScreen")
+        }
+
+        func bannerViewDidDismissScreen(_ bannerView: GADBannerView) {
+            print("bannerViewDidDismissScreen")
+        }
+    }
+}
+
+// Extending Application to get RootView..
+extension UIApplication {
+    func getRootViewController() -> UIViewController {
+
+        guard let screen = self.connectedScenes.first as? UIWindowScene else {
+            return .init()
+        }
+
+        guard let root = screen.windows.first?.rootViewController else {
+            return .init()
+        }
+
+        return root
+    }
+}
+*/
 
 func toggleTorch(on: Bool) {
     guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
